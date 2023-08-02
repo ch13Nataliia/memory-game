d4c77c116356efd643f8ed1c484bb96a51994727
Nataliia Tsirul <tcirulnatalya@gmail.com>
2023-08-02 13:15:32 +0100

    added functionality

--- main.js
+++ main.js
@@ -16,20 +16,59 @@ const tileCount = colorsPickList.length;
 let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfMove = false;
+
 function buildTile(color) {
- const element = document.createElement("div");
- element.classList.add("tile");
- element.setAttribute("data-color", color);
- return element;
+  const element = document.createElement('div');
+
+  element.classList.add('tile');
+  element.setAttribute('data-color', color);
+  element.setAttribute('data-revealed', 'false');
+
+  element.addEventListener('click', () => {
+    const revealed = element.getAttribute('data-revealed');
+
+    if (awaitingEndOfMove || revealed === 'true' || element === activeTile) {
+      return;
+    }
+    element.style.backgroundColor = color;
+    if (!activeTile) {
+      activeTile = element;
+      return;
+    }
+
+    const colorToMatch = activeTile.getAttribute('data-color');
+
+    if (colorToMatch === color) {
+      activeTile.setAttribute('data-revealed', 'true');
+      element.setAttribute('data-revealed', 'true');
+      activeTile = null;
+      awaitingEndOfMove = false;
+      revealedCount += 2;
+
+      if (revealedCount === tileCount) {
+        alert('You win');
+      }
+      return;
+    }
+
+    awaitingEndOfMove = true;
+    setTimeout(() => {
+      element.style.backgroundColor = null;
+      activeTile.style.backgroundColor = null;
+      awaitingEndOfMove = false;
+      activeTile = null;
+    }, 1000);
+  });
+  return element;
 }
 
-// Build gemae tiles
+// Build game tiles
 
 for (let i = 0; i < tileCount; i++) {
-  const randomIndex = Math.floor(Math.random() *colorsPickList.length);
+  const randomIndex = Math.floor(Math.random() * colorsPickList.length);
   const color = colorsPickList[randomIndex];
-const tile = buildTile(color);
+  const tile = buildTile(color);
   colorsPickList.splice(randomIndex, 1);
 
   tilesContainer.appendChild(tile);
-};
+}
